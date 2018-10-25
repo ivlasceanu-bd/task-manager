3157c4cca631b03148fa02e1c57f576dc6897ba3
Irina Vlasceanu <irinav0402@gmail.com>
2018-10-25 16:33:14 +0300

    get projects from firebase

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import Notifications from './Notifications'
 import TaskList from '../tasks/TaskList'
 import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 class Dashboard extends Component {
 
   render() {
-    // console.log(this.props);
     const { projects } = this.props;
 
     return (
@@ -25,9 +26,16 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return {
-    projects: state.project.projects
+    // projects: state.project.projects
+    projects: state.firestore.ordered.projects
   }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'projects' }
+  ])
+)(Dashboard)
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,11 +10,18 @@ const initState = {
 
 const projectReducer = (state = initState, action) => {
   switch( action.type ) {
+
     case 'CREATE_PROJECT' : 
       console.log('created project', action.project);
-      break;
+      return state;
+
+    case 'CREATE_PROJECT_ERROR' :
+      console.log('oops something went wrong', action.err);
+      return state;
+
+    default: 
+      return state;
   }
-  return state;
 }
 
 export default projectReducer;
