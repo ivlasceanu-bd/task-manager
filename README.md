bfd50cf3ed5acf9b42d81bb82c8f8542fae804e0
Irina Vlasceanu <irinav0402@gmail.com>
2018-10-31 17:01:24 +0200

    deploy to Firebase and update readme

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 Below you will find some information on how to perform common tasks.<br>
 You can find the most recent version of this guide [here](https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md).
 
+Firebase url:
+https://task-manager-946ad.firebaseapp.com
+
 ## Table of Contents
 
 - [Updating to New Releases](#updating-to-new-releases)
---
+++ dist/precache-manifest.4bdbb768073c7864c757fb5ef1e764da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a090061f211408eef0cc",
+    "url": "/static/js/main.a090061f.chunk.js"
+  },
+  {
+    "revision": "b2a6b1db00225e9247b0",
+    "url": "/static/js/1.b2a6b1db.chunk.js"
+  },
+  {
+    "revision": "a090061f211408eef0cc",
+    "url": "/static/css/main.d937c014.chunk.css"
+  },
+  {
+    "revision": "0bf2a0b41ead494cef58ec038389ce95",
+    "url": "/index.html"
+  }
+];
